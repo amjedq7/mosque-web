fd69114ea79490907823e97f09841a3510ba507e
amjedq7 <amjedq7.code@gmail.com>
2026-05-26 14:52:34 +0200

    almost working (laguages)

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage() {
+  return (
+    <>
+      <h2>Get in Touch</h2>
+      <p>If you have any questions about Islam or our community, please reach out to us.</p>
+      <p>Email: <a href="mailto:info@mosque.org" style={{color: 'var(--white)'}}>info@mosque.org</a></p>
+    </>
+  );
+}
