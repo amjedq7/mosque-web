f710a89d027febfb521ac52b2fabfb19056a47e8
amjedq7 <amjedq7.code@gmail.com>
2026-06-22 18:30:50 +0200

    fix: remove unused useEffect import to pass Netlify build

--- src/contexts/TranslationContext.tsx
+++ src/contexts/TranslationContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState } from 'react';
 import type { ReactNode } from 'react';
 import { translations, type Translation } from '../translations';
 
